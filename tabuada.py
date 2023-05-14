ed89e5d71cf82788df3bf609f1bb26b6601af0a1
DevJoao <joaovitorlucasdev@gmail.com>
2023-05-14 09:07:47 -0300

    1.0

--- tabuada.py
+++ tabuada.py
@@ -3,5 +3,5 @@ numeros = list(range(1,11))
 for numero in numeros:
     print('Tabuada do {}:'.format(numero))
     for outro_numero in numeros:
-        print('{:^30}'.format(numero * outro_numero))
-    print('--------------')
+        print('{} X {} = {}'.format(numero, outro_numero, numero * outro_numero))
+    print('=' * 18)
